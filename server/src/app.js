07eb8c9af74f80b9f2a4ce2e4c9e416277e18a87
himanshu dubey <himanshudubey0505@gmail.com>
2025-07-17 16:36:54 +0530

    fourth commit

--- server/src/app.js
+++ server/src/app.js
@@ -9,10 +9,14 @@ const app = express();
 const httpServer = createServer(app);
 
 
+const allowedOrigins = [
+  'http://localhost:5173',              // for local dev
+  'https://chat-app-abq8.vercel.app'    // your deployed frontend
+];
 
 const io = new Server(httpServer, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: allowedOrigins,
     methods: ["GET", "POST"],
     credentials: true,
   },
