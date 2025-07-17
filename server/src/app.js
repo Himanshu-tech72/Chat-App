342bb15fd744b992a875f43eac1258e2d415a17c
himanshu dubey <himanshudubey0505@gmail.com>
2025-07-17 16:49:21 +0530

    fourth commit

--- server/src/app.js
+++ server/src/app.js
@@ -11,7 +11,7 @@ const httpServer = createServer(app);
 
 const allowedOrigins = [
   'http://localhost:5173',              // for local dev
-  'https://chat-app-abq8.vercel.app'    // your deployed frontend
+  'https://chat-app-git-main-himanshu-dubeys-projects-de07467d.vercel.app/'    // your deployed frontend
 ];
 
 const io = new Server(httpServer, {
